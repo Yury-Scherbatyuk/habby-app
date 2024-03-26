e0eac73ca8b43041349b75b57b6a9f1cb2f4a8c5
Yury-S <yury.scherbatyuk.dev@gmail.com>
2024-03-27 02:42:41 +0300

    HA-3: Add button amination

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -2,27 +2,79 @@ import { StyleSheet, View, Text, Pressable, Animated } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
-
+import { useState } from 'react';
 
 export default function TopBar() {
+    const [settingsGearAnimation] = useState(new Animated.Value(0))
+    const [plusIconAnimation] = useState(new Animated.Value(0))
     const [fontsLoaded] = useFonts({
         'Outfit-Bold': require("../assets/fonts/Outfit-Bold.ttf")
     })
+    const gearAnimationDuration = 800
+    const plusAnimationDuration = 250
 
     if (!fontsLoaded) {
         return <Text>noooo</Text>
     }
 
+    const animateSettingsGear = () => {
+        Animated.timing(settingsGearAnimation, {
+            toValue: 1,
+            duration: gearAnimationDuration,
+            useNativeDriver: true
+        }).start(() => {
+            // set initial state after finish
+            settingsGearAnimation.setValue(0)
+        })
+    }
+
+    const interpolatedGearAnimation = settingsGearAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "90deg"]
+    })
+
+    const animatePlusIcon = () => {
+        Animated.timing(plusIconAnimation, {
+            toValue: 1,
+            duration: plusAnimationDuration,
+            useNativeDriver: true
+        }).start(() => {
+            // plusIconAnimation.setValue(0)
+            Animated.timing(plusIconAnimation, {
+                toValue: 0,
+                duration: plusAnimationDuration,
+                useNativeDriver: true
+            }).start()
+        })
+    }
+
+    const interpolatedPlusAnimation = plusIconAnimation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [1, 1.15]
+    })
+
     return(
         <View style={styles.container}>
-            <Pressable>
-                <Feather name="settings" size={34} color="white" />
-            </Pressable> 
+            <Animated.View
+                style={{
+                    transform: [{rotate: interpolatedGearAnimation}]
+                }}
+            >
+                <Pressable onPress={animateSettingsGear}>
+                    <Feather name="settings" size={34} color="white" />
+                </Pressable> 
+            </Animated.View>
             <Text style={styles.heading}>Habby</Text>
             <View style={styles.space} />
-            <Pressable>
-                <AntDesign name="pluscircleo" size={31} color="white" />
-            </Pressable>
+            <Animated.View
+                style={{
+                    transform: [{scale: interpolatedPlusAnimation}]
+                }}
+            >
+                <Pressable onPress={animatePlusIcon}>
+                    <AntDesign name="pluscircleo" size={31} color="white" />
+                </Pressable>
+            </Animated.View>
         </View>
     )
 }
